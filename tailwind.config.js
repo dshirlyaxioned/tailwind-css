44f16a07fba0dd34d66fd20d64d437954530278e
dshirlyaxioned <shirlyd@axioned.com>
2022-03-25 19:47:11 +0530

    bakya proj commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        xsm: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+}
